238005622a3e38789248294d4683202f50c864fe
Afshin Zarei <afshin.zarei2000@gmail.com>
2025-02-22 20:45:50 +0330

    Added a configuration to set Mongoose to use strictQuery: false

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -4,6 +4,8 @@ async function connectDB() {
   try {
     if (mongoose.connections[0].readyState) return;
 
+    mongoose.set("strictQuery", false);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ Connected to MongoDB");
